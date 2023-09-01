8c861c6c29d12c5f456f9323c158c70ec60f7438
avinashpatilcoder <133511337+avinashpatilcoder@users.noreply.github.com>
2023-09-01 10:28:46 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const structure = React.createElement("div", { id: "parent" }, 
+                  React.createElement("div", { id: "child" }, 
+                  [React.createElement("h1", {}, "Hello I am h1 tag"), React.createElement("h2", {}, "Hello I am h2 tag")]));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(structure);
