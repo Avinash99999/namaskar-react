fcb1c76d2829c827a9e05d137c98dff5a3d062d9
Avinash99999 <89960788+Avinash99999@users.noreply.github.com>
2023-09-14 12:21:20 +0530

    error resolved

--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -9,7 +9,8 @@ const ResturantCard = (props) => {
   return (
     <div className="card">
       <img
-        src={CDN_Link + receData.info.cloudinaryImageId}
+        //src={CDN_Link + receData?.info?.cloudinaryImageId}
+        src= "https://images.unsplash.com/photo-1532980400857-e8d9d275d858?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80"
         className="resimg"
       ></img>
       <h4>{name}</h4>
